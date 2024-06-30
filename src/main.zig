903ead7c157c72421db44ddd6565a52f9634135a
ckaznable <ckaznable@gmail.com>
2024-06-30 23:35:01 +0800

    feat: get diffrent/missing keys

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const cli = @import("./cli.zig");
-const util = @import("./util.zig");
 
 const json = @import("./json.zig");
 
@@ -32,7 +31,7 @@ pub fn main() !void {
     const base_keys = base_data.keys();
     std.debug.print("has {} keys in base json\n", .{base_keys.len});
 
-    var dirs = try util.walkDir(allocator, args.target);
+    var dirs = try walkDir(allocator, args.target);
     defer dirs.deinit();
 
     while (true) {
@@ -46,11 +45,40 @@ pub fn main() !void {
         var json_data = try json.readJsonFileToHashMap(allocator, entry.?.dir, file_name);
         defer json_data.deinit();
 
-        const data = json_data.value.object;
-        const keys = data.keys();
+        var data = json_data.value.object;
+        const data_keys = data.keys();
+
+        const keys = try allocator.alloc([]const u8, data_keys.len);
+        @memcpy(keys, data_keys);
+        defer allocator.free(keys);
+
         std.debug.print("{s} has {} keys\n", .{file_name, keys.len});
+
+        // diffrent keys
+        for (keys) |key| {
+            if (!base_data.contains(key)) {
+                _ = data.orderedRemove(key);
+            }
+        }
+
+        // missing keys
+        for (base_keys) |key| {
+            if (!data.contains(key)) {
+                try data.put(key, base_data.get(key).?);
+            }
+        }
+
+        std.debug.print("{s} keys after tidy: {}\n", .{file_name, data.keys().len});
     }
 
     std.debug.print("done\n", .{});
 }
 
+pub fn walkDir(allocator: std.mem.Allocator, dir_path: []const u8) !std.fs.Dir.Walker {
+    var dir = std.fs.cwd().openDir(dir_path, .{.iterate = true}) catch {
+        std.debug.print("target directory not exist", .{});
+        std.process.exit(1);
+    };
+
+    return try dir.walk(allocator);
+}
